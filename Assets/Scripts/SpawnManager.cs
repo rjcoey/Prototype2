e84f3c6ee4637868a589f5d09c25b8346ec87c6a
Robbie Coey <rjcoey@hotmail.co.uk>
2025-09-26 10:39:08 +0100

    Completed lesson 2.3
    Adjusted camera to orthographic
    Added animal spawning in random locations to s button press

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(
+                animalPrefabs[animalIndex],
+                spawnPos,
+                animalPrefabs[animalIndex].transform.rotation
+                );
+        }
+    }
+}
